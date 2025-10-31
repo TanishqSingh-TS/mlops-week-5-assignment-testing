aecff87b3b05da358b45ae8841002ba0cd3f952c
TanishqSingh-TS <tanishqsinghofficial2002@gmail.com>
2025-10-31 20:47:56 +0530

    Fix formatting of max_features parameter in model_train.py

--- model_train.py
+++ model_train.py
@@ -36,7 +36,7 @@ param_grid = {
     'max_depth': [None, 5, 10, 20, 30],
     'min_samples_split': [2, 5, 10],
     'min_samples_leaf': [1, 2, 4],
-    'max_features': [None, 'sqrt', 'log2']
+    'max_features': [None, 'sqrt', 'log2'],
     'class_weights':[None,'balanced']
 }
 
